c5bce3ef7c27074ad34a6fac61ea39face4534d3
easySoft <learnmore.cumi@gmail.com>
2018-02-01 12:10:33 +0700

    socket server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = process.env.port || 1122;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => {
+    console.log("user connected");
+
+    socket.on("disconnect", function() {
+        console.log("user disconnected");
+    });
+
+    socket.on("chat message", msg => {
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+    });
+});
+
+http.listen(port, function() {
+    console.log("Listening on port : " + port);
+});
